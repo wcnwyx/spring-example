4dd70c21e8c7b7ec0fa20aae0e920e211e026805
wucheng <wc@candypay.com>
2021-01-29 17:29:38 +0800

    'tx'

---
+++ spring-jdbc-tx/src/main/java/com/wcnwyx/spring/tx/example/TxConfig.java
@@ -0,0 +1,9 @@
+package com.wcnwyx.spring.tx.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TxConfig {
+}
